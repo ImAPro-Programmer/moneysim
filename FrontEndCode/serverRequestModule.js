2c868d2e687e2e6bef4a9de3c46b414ae14ec632
ImAPro-Programmer <95302237+ImAPro-Programmer@users.noreply.github.com>
2025-02-13 19:43:03 -0500

    createdServerRequest

--- FrontEndCode/serverRequestModule.js
+++ FrontEndCode/serverRequestModule.js
@@ -1,5 +1,21 @@
-
 let options = {
-    method: "POST"
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        userBalance: userBal
+    })
 }
-console.log(userBal);
+console.log("debugging purposes: " + userBal);
+async function makeRequest(){
+    try{
+        const response = await fetch("https://moneysimworker.coolreybansal.workers.dev/", options);
+        if(!response.ok) throw new Error(`Error: ${response.status}`);
+        const data = await response.json();
+        console.log("Data: " + data);
+    } catch(error){
+        console.error("Error found: " + error.message)
+    }
+}
+makeRequest();
