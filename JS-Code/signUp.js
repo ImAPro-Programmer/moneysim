f982c51fb85786ae3c0793d797740db7924c485e
ImAPro-Programmer <95302237+ImAPro-Programmer@users.noreply.github.com>
2025-02-16 01:52:45 -0500

    gdfgrsd

--- JS-Code/signUp.js
+++ JS-Code/signUp.js
@@ -2,6 +2,8 @@ let getSignUpUser = document.getElementById("userSignUp").value;
 let getSignUpPass = document.getElementById("passSignUp").value;
 let newUserDialog = document.getElementById("newUserEvent");
 document.getElementById("signUpButton").addEventListener("click", function(){
+    console.log(getSignUpUser);
+    console.log(getSignUpPass);
     signUpToDB()
     displayMainContent();
     newUserEvent.style.textContent = `New account created! username: ${getSignUpUser} and your password is: ${getSignUpPass}!`;
