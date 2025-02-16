5ad7cf79235bed0cbb611cc25524af1b7a3d03c8
ImAPro-Programmer <95302237+ImAPro-Programmer@users.noreply.github.com>
2025-02-16 01:51:15 -0500

    .value

--- JS-Code/signUp.js
+++ JS-Code/signUp.js
@@ -1,5 +1,5 @@
-let getSignUpUser = document.getElementById("userSignUp");
-let getSignUpPass = document.getElementById("passSignUp");
+let getSignUpUser = document.getElementById("userSignUp").value;
+let getSignUpPass = document.getElementById("passSignUp").value;
 let newUserDialog = document.getElementById("newUserEvent");
 document.getElementById("signUpButton").addEventListener("click", function(){
     signUpToDB()
