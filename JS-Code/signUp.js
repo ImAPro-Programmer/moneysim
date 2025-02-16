6e5095be0d8b6313923eb4b88f95386ef4749982
ImAPro-Programmer <95302237+ImAPro-Programmer@users.noreply.github.com>
2025-02-16 01:57:13 -0500

    tgdfh

--- JS-Code/signUp.js
+++ JS-Code/signUp.js
@@ -1,6 +1,7 @@
 let getSignUpUser = "";
 let getSignUpPass = "";
 let newUserDialog = document.getElementById("newUserEvent");
+let loginScreen = document.querySelector(".signUpMenu");
 document.getElementById("signUpButton").addEventListener("click", function(){
     getSignUpUser = document.getElementById("userSignUp").value;
     getSignUpPass = document.getElementById("passSignUp").value;
@@ -8,5 +9,6 @@ document.getElementById("signUpButton").addEventListener("click", function(){
     console.log(getSignUpPass);
     signUpToDB()
     displayMainContent();
+    loginScreen.style.display = "none";
     newUserEvent.style.textContent = `New account created! username: ${getSignUpUser} and your password is: ${getSignUpPass}!`;
 })
