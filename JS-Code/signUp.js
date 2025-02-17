925e6293d07512e203471ff3c6ae403229642853
ImAPro-Programmer <95302237+ImAPro-Programmer@users.noreply.github.com>
2025-02-17 13:59:44 -0500

    more debug bruh

--- JS-Code/signUp.js
+++ JS-Code/signUp.js
@@ -11,4 +11,5 @@ document.getElementById("signUpButton").addEventListener("click", function(){
     displayMainContent();
     loginScreen.style.display = "none";
     newUserEvent.style.textContent = `New account created! username: ${getSignUpUser} and your password is: ${getSignUpPass}!`;
+    console.log("debug for signup");
 })
