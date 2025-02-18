589727bef4181071846ff2e3ec6ee695dbab69eb
ImAPro-Programmer <95302237+ImAPro-Programmer@users.noreply.github.com>
2025-02-17 22:02:15 -0500

    thdsrf

--- JS-Code/login(front).js
+++ JS-Code/login(front).js
@@ -41,7 +41,7 @@ document.getElementById("loginButton").addEventListener("click", async function 
             newUserDialog.remove();
         }, 5000);
         finalUsernameToUse = usernameFromDB;
-    }else{
+    }else if(getResponse.success == false){
         console.log("Invalid username or password!");
         alert("Invalid username or password! (could be an interal error too, idk man)");
     }
