1d9d49fe458dee309b40921192469fe9847c429b
ImAPro-Programmer <95302237+ImAPro-Programmer@users.noreply.github.com>
2025-03-03 20:55:55 -0500

    rfgae

---
+++ JS-Code/handleRandomEvent.js
@@ -0,0 +1,21 @@
+let boostfromServer;
+let messageFromServer;
+async function randomEventGen(){
+    const response = await fetch("https://moneysimworker.coolreybansal.workers.dev/balanceInfo/randomEvent", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        credentials: "include"
+    });
+
+    if(!response.ok) throw new Error(`an error occured from randomEventGen! ${response.staus}`);
+
+    const serverReply = await response.json();
+
+    boostfromServer = serverReply.boosts;
+    messageFromServer = serverReply.message;
+    console.log(boostfromServer); //delete after production
+}
+
+setInterval(randomEventGen(), 5000);
