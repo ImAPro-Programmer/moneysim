97ca92f30d0e2ec0bba07c69459307935a95c0c5
Reyansh Bansal <coolreybansal@gmail.com>
2025-05-05 22:38:53 -0400

    )

--- FrontEndCode/saveUserData.js
+++ FrontEndCode/saveUserData.js
@@ -9,7 +9,7 @@ async function saveAllData() {
             credentials: "include",
             body: JSON.stringify({
                 userToFind: finalUsernameToUse,
-                saveBal: 50000,//,
+                saveBal: userBalFromServer,
             })
         });
 
