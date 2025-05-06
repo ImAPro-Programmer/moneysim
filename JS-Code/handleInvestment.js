7a0101b195ee6f584fe83f077b6b69566c025fff
Reyansh Bansal <coolreybansal@gmail.com>
2025-05-05 21:30:12 -0400

    playing with api-frontend

--- JS-Code/handleInvestment.js
+++ JS-Code/handleInvestment.js
@@ -20,6 +20,7 @@ async function handleInvestments(stock){
     const coolDownState = result.isCooldown;
 
     console.log("cooldown is " + coolDownState);
+    console.log("balance from serv:" + result.handleInvestmentFunc.balance);
 }
 
 
