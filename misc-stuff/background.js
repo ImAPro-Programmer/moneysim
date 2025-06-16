a45248420ea925f85efff1f763d7b1d7c6c367b5
Reyansh Bansal <coolreybansal@gmail.com>
2025-06-16 17:27:00 -0400

    background music

---
+++ misc-stuff/background.js
@@ -0,0 +1,23 @@
+let playlist = [
+    "chill_lobby.mp3",
+    "nostalgia_lobby.mp3",
+    "sherbet_lobby.mp3",
+    "shop.mp3",
+    "video_game_lobby.mp3",
+];
+
+let current = 0;
+let audio = new Audio();
+audio.src = playlist[current];
+audio.autoplay = true;
+audio.onended = () => {
+    current ++;
+    if (current < playlist.length){
+        audio.src = playlist[current];
+        audio.play();
+    } else {
+        current = 0;
+        audio.src = playlist[current];
+        audio.play();
+    }
+};
